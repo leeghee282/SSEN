bf2e9c724d27b85b6c49685232a9228337fa0f74
이근희 <report2829@naver.com>
2022-09-16 10:28:33 +0900

    feat: 스켈레톤 코드 생성

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>{/* <Route path="/" element={< />} /> */}</Routes>
+    </Router>
+  );
+}
+
+export default App;
