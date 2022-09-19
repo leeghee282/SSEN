a6448f7188a1593edc217de0e05bb6c896a9ec26
안정현 <kkk0125-@naver.com>
2022-09-19 12:06:03 +0900

    feat: 메인페이지 날짜 선택 및 국가 선택 버튼

---
+++ frontend/src/pages/MainPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from '../../components/Calendar';
+
+const UserLogin = () => {
+  return (
+    <>
+    <Calendar/>
+    </>
+  );
+};
+
+export default UserLogin;
