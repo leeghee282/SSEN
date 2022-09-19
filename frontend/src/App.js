35032ee529bba2d812b60aee937bbe22b51ecda0
안정현 <kkk0125-@naver.com>
2022-09-19 11:23:33 +0900

    feat: 라우터 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Main from "./pages/MainPage";
+import Signup from "./pages/SignupPage";
+import Login from "./pages/LoginPage";
+import Profile from "./pages/ProfilePage";
+import ProfileUpdate from "./pages/ProfileUpdatePage";
+
 
 function App() {
   return (
     <Router>
-      <Routes>{/* <Route path="/" element={< />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/profileupdate" element={<ProfileUpdate/>} />
+      </Routes>
     </Router>
   );
 }
