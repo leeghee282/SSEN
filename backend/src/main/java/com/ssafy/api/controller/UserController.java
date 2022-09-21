11246060a96cb72054bc3629753ad4e26c55cbd8
yujeonge <jeong000746@gmail.com>
2022-09-21 20:59:59 +0900

    fix : 닉네임 중복 검사 버그 수정

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -46,13 +46,7 @@ public class UserController {
         System.out.println("sign in controller");
         //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
         User user = userService.createUser(registerInfo);
-<<<<<<< Updated upstream
-
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
-=======
-        return ResponseEntity.status(200).body(UserRes.of(200, "Success", user));
-      //  return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
->>>>>>> Stashed changes
     }
 
     @GetMapping("/id-info/{userId}")
