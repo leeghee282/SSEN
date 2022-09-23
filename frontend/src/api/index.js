26fb2f0dbc4ea9093659c3f33a289749ab46eecd
이근희 <report2829@naver.com>
2022-09-23 17:31:13 +0900

    release: 배포 테스트

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:8080";
-// export const baseURL = "https://j7e204.p.ssafy.io";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://j7e204.p.ssafy.io";
