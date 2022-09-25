2fd75d1ad8bc1401f224c12eab95dacd72e41ee0
이근희 <report2829@naver.com>
2022-09-25 15:49:46 +0900

    fix: api

--- backend/backend_API/src/main/java/com/ssafy/db/repository/CurrencyCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.BankExchangeRate;
-import com.ssafy.db.entity.CurrencyCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyCategoryRepository extends JpaRepository<CurrencyCategory, Long> {
-    public CurrencyCategory findByCode(String code);
-}
