b84e2d16767b8236aa5a4efda7c0b6d79cc35978
이근희 <report2829@naver.com>
2022-09-26 08:56:13 +0900

    fix

--- frontend/src/_actions/types.js
+++
@@ -1,6 +0,0 @@
-export const GET_EXCHANGE_RATE = "get_exchange_rate";
-export const GET_BANKS_INFO = "get_banks_info";
-
-export const GET_CHART_DATES = "get_chart_dates";
-export const GET_CHART_CODE = "get_chart_code";
-export const GET_DATA = "get_data";
--- frontend/src/api/index.js
+++
@@ -1,2 +0,0 @@
-export const baseURL = "http://localhost:8080";
-// export const baseURL = "http://j7e204.p.ssafy.io:8080";
