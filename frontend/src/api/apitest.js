9a4bc0fbc7fd8aa899f730c6e1718075eec00517
이근희 <report2829@naver.com>
2022-09-26 11:48:20 +0900

    release: 테스트

--- frontend/src/api/apitest.js
+++ frontend/src/api/apitest.js
@@ -46,10 +46,19 @@ function Apitest() {
     console.log(ans);
   };
 
+  const getnews = () => {
+    const ans = axios
+      .get("https://j7e204.p.ssafy.io/news/keyword/2022-09-01/2022-09-05")
+      .then((res) => res.data);
+
+    console.log(ans);
+  };
+
   return (
     <div>
       <button onClick={getchat}>ddd</button>
       <button onClick={getcurr}>ddd</button>
+      <button onClick={getnews}>ddd</button>
     </div>
   );
 }
