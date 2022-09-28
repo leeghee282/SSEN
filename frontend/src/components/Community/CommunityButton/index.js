ee9d0ad7289fc2b2bb44389d62a3c654a56f65f1
배지우 <wldn102@naver.com>
2022-09-28 21:46:36 +0900

    feat:css채팅수정

--- frontend/src/components/Community/CommunityButton/index.js
+++ frontend/src/components/Community/CommunityButton/index.js
@@ -2,16 +2,24 @@
 import React, { useState } from "react";
 import CommunityList from "../CommunityList";
 import { Box } from "@mui/system";
-import { Button } from "@mui/material";
+import { Avatar, Button, Typography } from "@mui/material";
 import "./style.css";
 
 const CommunityButton = () => {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
   return (
     <>
 
 {!visible && (
       <Box sx={{ background: "#F5F5F5", height: "1000px", width: "450px" }}>
+        <Avatar 
+        sx={{width:"150px",height:"150px",position:"fixed"}}
+        onClick={() => {
+          setVisible(!visible);
+        }}
+        className="chat_main"
+        src='images/ssenchat.gif'>
+
         <button
           className="button_main"
           onClick={() => {
@@ -20,13 +28,16 @@ const CommunityButton = () => {
         >
           {visible ? "채팅창 닫기" : "채팅창 열기"}
         </button>
+        </Avatar>
 
         {visible && <CommunityList />}
       </Box>
       )}
       {visible && (
-      <Box sx={{ background: "#707173", height: "1000px", width: "450px" }}>
+      <Box sx={{ background: "#ABADB0", height: "1000px", width: "460px" }}>
+        <Avatar className="chat_title" src="images/ssenchat2.png" sx={{width:"300px",height:"300px"}}>  </Avatar>
         <button
+          position="absolute"
           className="button_main"
           onClick={() => {
             setVisible(!visible);
