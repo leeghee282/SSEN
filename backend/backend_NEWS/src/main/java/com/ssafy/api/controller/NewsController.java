fb0ec470f3c80c89142bb16d07e6869bcbbced4e
leesj8676 <leesj8676@gmail.com>
2022-09-30 13:23:23 +0900

    fix : 키워드로 뉴스 검색 api 필요 파라미터 수정
    날짜 없어도 되는걸로 수정

--- backend/backend_NEWS/src/main/java/com/ssafy/api/controller/NewsController.java
+++ backend/backend_NEWS/src/main/java/com/ssafy/api/controller/NewsController.java
@@ -87,14 +87,12 @@ public class NewsController {
         return new ResponseEntity<>(keywordList, HttpStatus.OK);
     }
 
-    @GetMapping("/search/{keyword}/{start_date}/{end_date}")
-    @ApiOperation(value = "키워드로 뉴스 검색", notes = "<strong>시작 날짜, 종료 날짜, 키워드</strong>를 입력해서 해당 범위의 키워드를 포함하는 뉴스 목록을 조회한다. (하루만 조회할 경우 start_date만 입력해도 가능)")
+    @GetMapping("/search/{keyword}")
+    @ApiOperation(value = "키워드로 뉴스 검색", notes = "<strong> 키워드</strong>를 입력해서 이미 선택했던 날짜의 범위의 키워드를 포함하는 뉴스 목록을 조회한다. (하루만 조회할 경우 start_date만 입력해도 가능)")
     @ApiResponses({@ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 400, message = "조회 결과 없음(날짜, 하둡 등 확인필요)"),
             @ApiResponse(code = 500, message = "서버 오류")})
     public ResponseEntity<List<NewsRes>> getNewsByKeywordAndDates(
-            @ApiParam(value = "시작 날짜(YYYY-MM-DD)", required = true) @PathVariable("start_date") String startDate,
-            @ApiParam(value = "종료 날짜(YYYY-MM-DD)", required = false) @PathVariable("end_date") String endDate,
             @ApiParam(value = "키워드", required = true) @PathVariable("keyword") String keyword
     ) {
 
