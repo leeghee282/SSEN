e4a8b39f7207bf00bd27657e1f57d238033b5444
leesj8676 <leesj8676@gmail.com>
2022-10-03 00:53:03 +0900

    feat : 전체 뉴스에서 키워드 검색 API 구현 (navbar용)

--- backend/backend_NEWS/src/main/java/com/ssafy/api/controller/NewsController.java
+++ backend/backend_NEWS/src/main/java/com/ssafy/api/controller/NewsController.java
@@ -4,6 +4,7 @@ import com.jcraft.jsch.Session;
 import com.ssafy.api.request.KeywordReq;
 import com.ssafy.api.response.KeywordRes;
 import com.ssafy.api.response.NewsRes;
+import com.ssafy.api.response.NewsRes2;
 import com.ssafy.api.response.PastRes;
 import com.ssafy.api.service.KeywordService;
 import com.ssafy.common.util.jsch.SSHUtil;
@@ -92,10 +93,10 @@ public class NewsController {
     }
 
     @GetMapping("/search/{keyword}")
-    @ApiOperation(value = "키워드로 뉴스 검색", notes = "<strong> 키워드</strong>를 입력해서 이미 선택했던 날짜의 범위의 키워드를 포함하는 뉴스 목록을 조회한다. (하루만 조회할 경우 start_date만 입력해도 가능)")
+    @ApiOperation(value = "키워드로 뉴스 검색", notes = "<strong> 키워드</strong>를 입력해서 이미 선택했던 날짜의 범위의 키워드를 포함하는 뉴스 목록을 조회한다.")
     @ApiResponses({@ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 400, message = "조회 결과 없음(날짜, 하둡 등 확인필요)"),
-            @ApiResponse(code = 500, message = "서버 오류")})
+            @ApiResponse(code = 500, message = "서버 오류(로그 확인 바람)")})
     public ResponseEntity<List<NewsRes>> getNewsByKeywordAndDates(
             @ApiParam(value = "키워드", required = true) @PathVariable("keyword") String keyword
     ) {
@@ -129,6 +130,45 @@ public class NewsController {
         Collections.reverse(newsResList);
         return new ResponseEntity<>(newsResList, HttpStatus.OK);
     }
+
+    @GetMapping("/search2/{keyword}")
+    @ApiOperation(value = "키워드로 뉴스 검색", notes = "<strong> 키워드</strong>를 입력해서 이미 선택했던 날짜의 범위의 키워드를 포함하는 뉴스 목록을 조회한다.")
+    @ApiResponses({@ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 400, message = "조회 결과 없음(날짜, 하둡 등 확인필요)"),
+            @ApiResponse(code = 500, message = "서버 오류(로그 확인 바람)")})
+    public ResponseEntity<List<NewsRes2>> getNewsByKeywordAndDates2(
+            @ApiParam(value = "키워드", required = true) @PathVariable("keyword") String keyword
+    ) {
+
+        List<NewsRes2> newsResList = new ArrayList<>();
+
+        Session jschSession = sshUtil.sessionConnect();
+
+        String cmd = "/home/hadoop/hadoop/bin/hadoop jar ~/mapreduce/ssen.jar news2 " + keyword + " news_in/news news_out > /dev/null 2>&1 &&  /home/hadoop/hadoop/bin/hdfs dfs -cat news_out/*";
+
+//        System.out.println("확인하기4 " + cmd);
+        String response = sshUtil.cmd(jschSession, cmd);
+        StringTokenizer st = new StringTokenizer(response, "\n");
+
+        if (!st.hasMoreTokens())
+            return ResponseEntity.status(400).body(null);
+
+        while (st.hasMoreTokens()) {
+            String news = st.nextToken(); // 개별 기사
+            StringTokenizer st2 = new StringTokenizer(news, ",");
+            while (st2.hasMoreTokens()) {
+                String time = st2.nextToken();
+                String press = st2.nextToken().trim();
+                String title = st2.nextToken();
+                String url = st2.nextToken();
+                NewsRes2 newsRes = NewsRes2.of(title, press, time, url);
+                newsResList.add(newsRes);
+            }
+        }
+        Collections.reverse(newsResList);
+        return new ResponseEntity<>(newsResList, HttpStatus.OK);
+    }
+
 
     @PostMapping("/past")
     @ApiOperation(value = "비슷한 키워드 분포를 가지는 날짜 조회", notes = "<strong>현재 선택한 날짜의 키워드 분포</strong>를 통해서 비슷한 키워드 분포를 가진 날짜-유사도 목록을 조회한다.")
