52ebf03c651fb47ce2aa366640412442fb3c431f
김유정 <jeong746@naver.com>
2022-10-04 10:39:35 +0900

    fix - 웹소켓

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -93,7 +93,7 @@ const Header = () => {
 
   // 유정 추가
   // 웹소켓 연결
-  const webSocket = new WebSocket("wss://j7e204.p.ssafy.io:8080/ssen");
+  const webSocket = new WebSocket("ws://j7e204.p.ssafy.io:8080/ssen");
 
   useEffect(() => {
     webSocket.onopen = function () {};
