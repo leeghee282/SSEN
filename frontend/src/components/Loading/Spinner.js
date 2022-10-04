4550fecb7d0cab6b556fe8989e7720e1db6613a7
이근희 <report2829@naver.com>
2022-10-04 23:02:52 +0900

    release

--- frontend/src/components/Loading/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import MagnifyIcon from "../../assets/images/Magnify.gif";
-
-const Spinner = (props) => {
-  return <img src={MagnifyIcon} alt="로딩중" />;
-};
-export default Spinner;
