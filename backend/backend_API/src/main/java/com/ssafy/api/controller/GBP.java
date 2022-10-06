ba9486bd58b42fb8fac87ad5d6859652971e6462
yujeonge <jeong000746@gmail.com>
2022-10-07 00:55:03 +0900

    fix : 실시간 환율 시간수정

--- backend/backend_API/src/main/java/com/ssafy/api/controller/GBP.java
+++ backend/backend_API/src/main/java/com/ssafy/api/controller/GBP.java
@@ -40,7 +40,7 @@ public class GBP {
             regdate1 = liveCurrencyRes.getRegdate();
         }
     }
-    @Scheduled(fixedRate = 60*1000)
+    @Scheduled(fixedRate = 10*1000)
     public void push() {
         //실시간 환율 받아오기
         LiveCurrencyRes liveCurrencyRes = liveCurrencyService.findLiveCurrencyByCCUid("GBP");
